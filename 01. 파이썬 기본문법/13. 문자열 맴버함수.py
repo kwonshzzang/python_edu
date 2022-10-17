121bf42d86eb543a62210b2b13954fdb14e8ca5e
kwonshzzang <105833021+kwonshzzang@users.noreply.github.com>
2022-10-17 16:43:26 +0900

    2022.10.17

---
+++ 01. 파이썬 기본문법/13. 문자열 맴버함수.py
@@ -0,0 +1,19 @@
+# 파이썬의 모든 타입은 객체
+# 객체: 속성 + 메소드
+# 객체.속성, 객체.메소드(인자)
+
+
+a = 10
+s = ' abcdef '
+
+s1 = '###abc###'
+s2 = 'hello korea'
+s3 = 'abc def ghi'
+s4 = '000-111-222'
+
+print(s)
+print(s.strip())
+print(s1.strip('#'))
+print(s2.replace('hello', 'love'))
+print(s3.split())
+print(s4.split('-'))
